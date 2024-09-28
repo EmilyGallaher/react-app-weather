bbac54b79b0b814478fcc0f80a4ab88d0f0e39fb
wubbie88 <gallaher.emily@yahoo.com>
2024-09-27 22:47:02 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
